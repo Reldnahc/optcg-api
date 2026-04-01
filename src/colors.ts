5ca262b216cebe8bb0236bff54e124983774d586
cwmle <cwmlee00@gmail.com>
2026-03-31 21:14:30 -0500

    Add search aliases

--- src/colors.ts
+++ src/colors.ts
@@ -1,10 +1,16 @@
 const COLOR_MAP: Record<string, string> = {
   red: "Red",
+  r: "Red",
   green: "Green",
+  g: "Green",
   blue: "Blue",
+  u: "Blue",
   purple: "Purple",
+  p: "Purple",
   black: "Black",
+  b: "Black",
   yellow: "Yellow",
+  y: "Yellow",
 };
 
 export function normalizeColorFilter(value: string): string[] {
